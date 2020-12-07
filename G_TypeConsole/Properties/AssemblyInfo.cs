d4c62eca809c9825ddf41cdfe33454eb4fa396f6
NotImplementedLife <not.implemented.life@gmail.com>
2020-12-07 18:08:45 +0200

    repairs

--- G_TypeConsole/Properties/AssemblyInfo.cs
+++ G_TypeConsole/Properties/AssemblyInfo.cs
@@ -1,6 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
-using System.Windows;
 
 [assembly: AssemblyTitle("G_TypeConsole")]
 [assembly: AssemblyDescription("Console-like generator for FlipnoteDesktop.")]
@@ -8,5 +7,6 @@ using System.Windows;
 [assembly: AssemblyProduct("G_TypeConsole")]
 [assembly: AssemblyCopyright("Copyright © N.I.L 2020")]
 [assembly: ComVisible(false)]
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
